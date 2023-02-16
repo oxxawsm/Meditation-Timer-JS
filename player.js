a83a17e3e10f681fb357e8adf78e258c409cda17
oxxawsm <ellena.titenko@gmail.com>
2023-02-17 00:55:43 +0300

    Улучшено воспроизведение музыки

--- player.js
+++ player.js
@@ -1,43 +1,119 @@
-let player = document.getElementById("player");
-let progress = document.getElementById("progress");
-let playbtn = document.getElementById("playbtn");
+let track_name = document.querySelector('.track-name');
+let track_artist = document.querySelector('.track-artist');
 
-const playpause = function () {
-  if (player.paused) {
-    player.play();
-  } else {
-    player.pause();
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
+
+let seek_slider = document.querySelector('.seek_slider');
+
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+
+let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+    {
+      name : 'Freezing',
+      artist : 'Meydän',
+      music : 'https://www.chosic.com/wp-content/uploads/2022/01/Meydan-Freezing-but-warm.mp3'
+    },
+    {
+      name : 'City Lights',
+      artist : 'Ghostriffer Official',
+      music : 'https://www.chosic.com/wp-content/uploads/2022/07/Ghostrifter-Official-City-Lights.mp3'
+    },
+    {
+      name : 'MANTRA',
+      artist : 'Alex-Productions',
+      music : 'https://www.chosic.com/wp-content/uploads/2022/08/MANTRON.mp3'
   }
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
 }
 
-playbtn.addEventListener("click", playpause);
-
-player.onplay = function () {
-  playbtn.classList.remove("fa-play");
-  playbtn.classList.add("fa-pause");
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
 }
 
-player.onpause = function () {
-  playbtn.classList.add("fa-play");
-  playbtn.classList.remove("fa-pause");
+function playpauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
 }
-
-player.ontimeupdate = function () {
-  let ct = player.currentTime;
-  current.innerHTML = timeFormat(ct);
-
-  // let duration = player.duration;
-  // prog = Math.floor((ct * 100) / duration);
-  // progress.style.setProperty("--progress", prog + "%");
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+function nextTrack(){
+    if (track_index < music_list.length - 1 && isRandom === false) {
+        track_index += 1;
+    } else if (track_index < music_list.length - 1 && isRandom === true) {
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    } else {
+        track_index = 0;
+    }
 
-function timeFormat(ct) {
-  minutes = Math.floor(ct / 60);
-  seconds = Math.floor(ct % 60);
+    loadTrack(track_index);
+    playTrack();
+}
+function prevTrack(){
+    if (track_index > 0) {
+        track_index -= 1;
+    } else {
+        track_index = music_list.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function seekTo(){
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+function setUpdate(){
+    let seekPosition = 0;
+    if (!isNaN(curr_track.duration)) {
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
 
-  if (seconds < 10) {
-    seconds = "0"+ seconds;
-  }
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-  return minutes + ":" + seconds;
+        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
 }
